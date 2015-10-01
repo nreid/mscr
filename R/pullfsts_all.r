d7136fcc53c39a68d7c0da54ece2a6acd294e788
Noah Reid <noah.reid@gmail.com>
2015-09-30 19:35:15 -0700

    rcontml changes

---
+++ R/pullfsts_all.r
@@ -0,0 +1,13 @@
+pullfstsall<-function(reg){
+	
+	fil<-list.files("~/popgen/variants/bowfree/ALL1/","wcfst.gz$")
+	com<-gsub(".wcfst.*","",fil)
+	fil<-paste("~/popgen/variants/bowfree/ALL1/",fil,sep="")
+
+	for(i in 1:length(fil)){
+
+		
+
+		}
+	
+	}
