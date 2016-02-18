bafb6a42d2fc474d326669c6e3ef319a9f74402a
Noah Reid <noah.reid@gmail.com>
2016-02-18 14:44:39 -0800

    windowed fst

---
+++ R/pullfsts_win.R
@@ -0,0 +1,16 @@
+#this function pulls Fst values from a given scaffold for a bgzipped and tabix-indexed table containing Fst for all snps in the genome. 
+	# in this case it pulls the values for four population pairs and puts each in a list. 
+	
+pullfsts_win<-function(scaffold, fst1="BI.NBH",fst2="BP.F",fst3="NYC.SH",fst4="ER.KC"){
+
+	res<-list()
+	res[[fst1]]<-read.table(pipe(paste("tabix /home/nreid/popgen/variants/bowfree/ALL1/", fst1,".wcfst.5kbwindow.bed.gz ", scaffold, collapse="", sep="")))
+	res[[fst2]]<-read.table(pipe(paste("tabix /home/nreid/popgen/variants/bowfree/ALL1/", fst2,".wcfst.5kbwindow.bed.gz ", scaffold, collapse="", sep="")))
+	res[[fst3]]<-read.table(pipe(paste("tabix /home/nreid/popgen/variants/bowfree/ALL1/", fst3,".wcfst.5kbwindow.bed.gz ", scaffold, collapse="", sep="")))
+	res[[fst4]]<-read.table(pipe(paste("tabix /home/nreid/popgen/variants/bowfree/ALL1/", fst4,".wcfst.5kbwindow.bed.gz ", scaffold, collapse="", sep="")))
+	for(i in 1:4){res[[i]][,4] <- as.numeric(as.character(res[[i]][,4]))}
+	for(i in 1:4){res[[i]][,5] <- as.numeric(as.character(res[[i]][,5]))}
+	for(i in 1:4){res[[i]][,6] <- as.numeric(as.character(res[[i]][,6]))}
+	return(res)
+	}
+
