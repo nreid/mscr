808fca7c76b06eeaaba0b4713d2db319d3b9512b
Noah Reid <noah.reid@gmail.com>
2016-02-18 10:34:58 -0800

    div stats

--- R/pulldiv.R
+++ R/pulldiv.R
@@ -1,4 +1,4 @@
-pulldiv <- function(scaf,stat="pi",win="5kb1kbwindow",popids=c("bi","bp","er","f","kc","nbh","nyc","sh")){
+pulldiv <- function(scaf,stat="pi",win="5kb1kbwindow",popids=c("bi","bp","er","f","kc","nbh","nyc","sh"),path="~/popgen/variants/bowfree/angsd/"){
 	pops <- popids
 	#tabix sh.pi.5kb1kbwindow.test.gz Scaffold9893
 	div <- list()
@@ -6,7 +6,7 @@ pulldiv <- function(scaf,stat="pi",win="5kb1kbwindow",popids=c("bi","bp","er","f
 
 		fn <- paste(pops[i],stat,win,"test.gz",sep=".",collapse=".")
 		print(fn)
-		fn <- paste("~/popgen/variants/bowfree/angsd/",fn,sep="",collapse="")
+		fn <- paste(path,fn,sep="",collapse="")
 		print(fn)
 		cline <- paste("tabix ",fn,scaf,sep=" ",collapse=" ")
 		print(cline)
@@ -17,7 +17,7 @@ pulldiv <- function(scaf,stat="pi",win="5kb1kbwindow",popids=c("bi","bp","er","f
 	out <- div[[1]][,c(2:3,6)]
 	for(i in 1:length(pops)){
 
-		out <- cbind(out,div[[i]][,4])
+		out <- cbind(out,as.numeric(as.character(div[[i]][,4])))
 
 		}
 
